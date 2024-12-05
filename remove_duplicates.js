7dd8af71428b19e6e58d5c2e1ee730c7a9e1debc
Tom Gagliano <tgaglianoll@gmail.com>
2024-12-04 19:30:18 -0500

    initial commit to test

---
+++ remove_duplicates.js
@@ -0,0 +1,13 @@
+// Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+
+function dups(array) {
+  var x = 0;
+  var newArray = [];
+
+  while (x < array.length) {
+    if (!x.includes(newArray(x)) {
+      x.push(newArray) } {
+        { else  {
+          x.push(newArray);
+        }
+      }
