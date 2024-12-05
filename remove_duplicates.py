d9d9dcf93f26bdecaaaec730262d1edffb7b1d62
Tom Gagliano <tgaglianoll@gmail.com>
2024-12-05 11:14:13 -0500

    solve remove duplicates problem in python

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,5 +1,6 @@
 # // Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
 
+
 def dups(array):
   x = 0
   newArray = []
