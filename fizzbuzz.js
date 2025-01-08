4a8aca78dedb75ba4a78e268963821071edddfa1
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-07 22:53:54 -0500

    solve FizzBuzz problem by printing values up to input number

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+// Problem: Write a function that prints numbers from 1 to 100. For multiples of 3, print "Fizz" instead of the number. For multiples of 5, print "Buzz". For numbers that are multiples of both 3 and 5, print "FizzBuzz".
+
+function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++)
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+}
+
+fizzBuzz(10);
